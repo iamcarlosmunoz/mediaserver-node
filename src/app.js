223bdf9ffe563093d94e2a89237dcd27003f2953
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-01-07 09:14:05 -0500

    Added path to client's html

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 import express from 'express'
+import path from 'path'
 import config from './config'
 import MoviesRoutes from './routes/movies.routes'
 
 const app = express()
 
 app.set('port', config.serverPort || 4000)
+
+app.use(express.static('client/build/'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'))
+})
 
 app.use('/api/movies',MoviesRoutes)
 
