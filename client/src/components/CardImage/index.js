b6821f00316d7bf904f5cb861f5675a86e841aca
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-01-09 14:14:26 -0500

    feature added tag media

--- client/src/components/CardImage/index.js
+++ client/src/components/CardImage/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useLocation } from 'wouter'
 import './styles.css'
 
-const CardImage = ({ typeMedia = 'movie', data }) => {
+const CardImage = ({ typeMedia = 'movies', data }) => {
 
     const [path, pushLocation] = useLocation()
 
@@ -18,6 +18,8 @@ const CardImage = ({ typeMedia = 'movie', data }) => {
                     <title>Informacion de la Película</title>
                     <path d="M295.84,146.049l-256-144c-4.96-2.784-11.008-2.72-15.904,0.128C19.008,5.057,16,10.305,16,16.001v288c0,5.696,3.008,10.944,7.936,13.824c2.496,1.44,5.28,2.176,8.064,2.176c2.688,0,5.408-0.672,7.84-2.048l256-144c5.024-2.848,8.16-8.16,8.16-13.952S300.864,148.897,295.84,146.049z"/>
                 </svg>
+                { (typeMedia === 'movies') &&  <span className="card-image__tag card-image__tag--color-movie">Película</span> }
+                { (typeMedia === 'series') &&  <span className="card-image__tag card-image__tag--color-serie">Serie</span> }
             </div>
         </div>
     )
