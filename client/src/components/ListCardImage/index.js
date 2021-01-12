aee0a882bfcc9fe6706631a0fbae690dc0397211
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-01-12 00:10:37 -0500

    skeleton cardimage added

--- client/src/components/ListCardImage/index.js
+++ client/src/components/ListCardImage/index.js
@@ -1,14 +1,15 @@
-import React, { useEffect, useState } from 'react'
-import './styles.css'
+import React, { useEffect, useState } from "react"
+import "./ListCardImage.css"
 
-import Carousel from 'react-elastic-carousel'
-import CardImage from '../CardImage'
+import Carousel from "react-elastic-carousel"
+import CardImage from "../CardImage"
+import SkeletonCardImage from "../SkeletonCardImage"
 
-import getMedia from '../../services/getMedia' 
+import getMedia from "../../services/getMedia" 
 
-const ListCardImage = ({ typeMedia = '' }) => {
+const ListCardImage = ({ typeMedia = "" }) => {
 
-    const [media, setMedia] = useState([])
+    const [media, setMedia] = useState(null)
 
     useEffect(function () {
         getMedia({ typeMedia })
@@ -19,18 +20,19 @@ const ListCardImage = ({ typeMedia = '' }) => {
 
     return ( 
         <Carousel 
-
             breakPoints={[
                 { width: 1, itemsToShow: 2, itemsToScroll: 1 },
                 { width: 550, itemsToShow: 3, itemsToScroll: 1 },
-                { width: 850, itemsToShow: 4, itemsToScroll: 1 },
-                { width: 1280, itemsToShow: 5, itemsToScroll: 1 },
+                { width: 850, itemsToShow: 5, itemsToScroll: 1 },
+                { width: 1270, itemsToShow: 5, itemsToScroll: 1 },
                 { width: 1450, itemsToShow: 7, itemsToScroll: 1 },
                 { width: 1750, itemsToShow: 7, itemsToScroll: 1 }
             ]}
         >
-            {media.map(element => <CardImage key={element.id} typeMedia={typeMedia} data={element}/>)}
+            { media && media.map(element => <CardImage key={element.id} typeMedia={typeMedia} data={element}/>) }
+            { !media && [1,2,3,4,5,6,7,8,9,10,11,12].map(element => <SkeletonCardImage key={element}/>) }
         </Carousel>
+        
     )
 }
 
---
+++ client/src/components/SkeletonCardImage/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./SkeletonCardImage.css"
+
+const SkeletonCardImage = () => {
+
+    return (
+        <div className="skeleton-card-image">
+            <div className="skeleton-card-image__label">
+            </div>
+        </div>
+    )
+}
+
+export default SkeletonCardImage
