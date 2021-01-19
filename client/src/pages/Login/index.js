a2e14ff9ee22ae4ce1b13cf470daa5b3349d6d19
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-01-19 15:19:37 -0500

    deleted imgbackground login page

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -8,8 +8,6 @@ import useAllUsers from "../../hooks/useAllUsers";
 import useGlobalUser from "../../hooks/useGlobalUser";
 import SkeletonCardUser from "./SkeletonCardUser";
 
-import ImgBackground from "../../assets/svg/Subtle-Stripes.svg"
-
 const Login = () => {
   const { error, isLoaded, users } = useAllUsers();
   const { login, isLoginLoaded, isLoginError, isLogged } = useGlobalUser();
@@ -30,7 +28,7 @@ const Login = () => {
   };
 
   return (
-    <div className="imposter imposter--center" style={{ backgroundImage: `url(${ImgBackground})` }}>
+    <div className="imposter imposter--center">
       <div className="login__wave login__wave--top">
         <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
           <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" className="shape-fill"></path>
